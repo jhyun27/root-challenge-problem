23849fd38c252909e56e07c3c35d7a2b44e73bf4
Joanne Hyun <jhyun27@gmail.com>
2019-11-25 09:06:54 -0500

    test for add trip to history

--- src/test/java/com/joinroot/DriverUnitTest.java
+++ src/test/java/com/joinroot/DriverUnitTest.java
@@ -1,13 +1,21 @@
 package com.joinroot;
 
+import java.time.LocalTime;
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.joinroot.triplogger.commands.Driver;
+import com.joinroot.triplogger.commands.Trip;
 
 public class DriverUnitTest {
 	
 	private final static String DRIVER_NAME = "Test Name";
+	private final static LocalTime START_TIME = LocalTime.now();
+	private final static LocalTime END_TIME = START_TIME.plusHours(1);
+	private final static double TRIP_MILES = 42.0;
 	
 	private Driver driver;
 	
@@ -18,7 +26,12 @@ public class DriverUnitTest {
 	
 	@Test
 	public void test_for_add_trip_to_history() {
+		Trip newTrip = new Trip(DRIVER_NAME, START_TIME, END_TIME, TRIP_MILES);
+		driver.addTripToHistory(newTrip);
 		
+		List<Trip> driverTripHistory = driver.getDriverTripHistory();
+		
+		Assert.assertEquals(newTrip, driverTripHistory.get(0));
 	}
 	
 	@Test
